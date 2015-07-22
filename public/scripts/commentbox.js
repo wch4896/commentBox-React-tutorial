f61853766507bb48387ae882953081a4f9d21c49
User <user@UserdeMacBook-Air.local>
2015-07-22 11:29:25 +0800

    - commentBox 1.0

--- public/scripts/commentbox.js
+++ public/scripts/commentbox.js
@@ -1,10 +1,48 @@
 var CommentBox = React.createClass({
+	
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.log(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	handleCommentSubmit: function(comment){
+		var comments = this.state.data;
+		var newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data){
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.log(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function(){
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
-				<CommentForm />
+				<CommentList data={this.state.data} />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
 		);
 	}
@@ -28,11 +66,25 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	handleSubmit: function(e){
+		e.preventDefault();
+		var author = React.findDOMNode(this.refs.author).value.trim();
+		var text = React.findDOMNode(this.refs.text).value.trim();
+		if (!author || !text) {
+			return ;
+		};
+		this.props.onCommentSubmit({author:author, text:text});
+		React.findDOMNode(this.refs.author).value = '';
+		React.findDOMNode(this.refs.text).value = '';
+		return ;
+	},
 	render: function(){
 		return (
-			<div className="commentForm">
-				Hello, world! I am a CommentForm.
-			</div>
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="Your name" ref="author" />
+				<input type="text" placeholder="Say something..." ref="text" />
+				<input type="submit" value="Post" />
+			</form>
 		);
 	}
 });
@@ -45,18 +97,14 @@ var Comment = React.createClass({
 				<h2 className="commentAuthor">
 					{this.props.author}
 				</h2>
-			<span dangerouslySetInnerHTML={{__html: rawMarkup}} />			
+			<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
 			</div>
 		);
 	}
 });
 
-var data = [
-  {author: "Pete Hunt", text: "This is one comment!!!!!!"},
-  {author: "Jordan Walke", text: "This is *another* comment......."}
-];
 
 React.render(
-	<CommentBox data={data} />,
+	<CommentBox url="comments.json" pollInterval={2000} />,
 	document.getElementById('content')
 );
--- server.js
+++ server.js
@@ -10,6 +10,24 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/comments.json', function(req, res){
+	fs.readFile('comments.json', function(err,data){
+		res.setHeader('Cache-Control', 'no-cache');
+		res.json(JSON.parse(data));
+	});
+});
+
+app.post('/comments.json', function(req, res){
+	fs.readFile('comments.json', function(err,data){
+		var comments = JSON.parse(data);
+		comments.push(req.body);
+		fs.writeFile('comments.json', JSON.stringify(comments, null, '\t'), function(err){
+			res.setHeader('Cache-Control', 'no-cache');
+			res.json(comments);
+		});
+	});
+});
+
 app.listen(app.get('port'), function(){
 	console.log('Server started: http://localhost:' + app.get('port') + '/')
 });
